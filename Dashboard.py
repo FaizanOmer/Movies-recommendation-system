bacfc0b4e8b6d9a644cd2a72eea8a8408c1a8ff9
Muhammad Faizan Omer <faizanomer2000@gmail.com>
2023-03-24 02:15:25 +0500

    Add files via upload

---
+++ Dashboard.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+st.title("Movie Recommender System ")
+
+selected_movie_name = st.selectbox(
+"Here you can select or search the movie of your own choice the recommender will recommend you five movies based on your selection.",
+movies['title'].values)
+
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
